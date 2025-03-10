bb34e41a581dff26eb623773621c581bd80811e6
Steve Hawkins <shawkins@redhat.com>
2025-03-10 09:38:06 -0400

    demonstration of header issue
    Signed-off-by: Steve Hawkins <shawkins@redhat.com>

--- getting-started/src/main/java/org/acme/getting/started/GreetingResource.java
+++ getting-started/src/main/java/org/acme/getting/started/GreetingResource.java
@@ -1,10 +1,13 @@
 package org.acme.getting.started;
 
+import org.jboss.resteasy.reactive.server.core.CurrentRequestManager;
+
 import jakarta.inject.Inject;
 import jakarta.ws.rs.GET;
 import jakarta.ws.rs.Path;
 import jakarta.ws.rs.Produces;
 import jakarta.ws.rs.core.MediaType;
+import jakarta.ws.rs.core.Response;
 
 @Path("/hello")
 public class GreetingResource {
@@ -23,5 +26,21 @@ public class GreetingResource {
     @Produces(MediaType.TEXT_PLAIN)
     public String hello() {
         return "hello";
+    }
+
+    @Path("/before")
+    @GET
+    public Response before() {
+        var context = CurrentRequestManager.get();
+        return Response.ok("Host: %s Scheme %s".formatted(context.getHttpHeaders().getRequestHeader("Host"),
+                context.getUriInfo().getBaseUri().getScheme())).build();
+    }
+
+    @Path("/after")
+    @GET
+    public Response after() {
+        var context = CurrentRequestManager.get();
+        return Response.ok("Scheme %s Host: %s".formatted(context.getUriInfo().getBaseUri().getScheme(),
+                context.getHttpHeaders().getRequestHeader("Host"))).build();
     }
 }
